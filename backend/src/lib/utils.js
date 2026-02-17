8d0bd1de32339b962874ca17c7aa74bba4c0ebe2
JahirulAmin <126044795+JahirulAmin@users.noreply.github.com>
2026-02-17 21:11:43 +0600

    welcome email complete

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ userId }, ENV.JWT_SECRET, {
     expiresIn: "15d"
   });
 
@@ -9,6 +10,6 @@ export const generateToken = (userId, res) => {
     httpOnly: true,
     sameSite: "strict",
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === "production" ? true : false
+    secure: ENV.NODE_ENV === "production" ? true : false
   });
 }
